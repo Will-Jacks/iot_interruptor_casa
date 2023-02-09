b804c718a249adba4789545556734dafadcfdd1a
Will-Jacks <lila.lilajackson@hotmail.com>
2023-02-08 21:34:10 -0300

    Adição do quarto PC

--- src/Components/Buttons/Buttons.jsx
+++ src/Components/Buttons/Buttons.jsx
@@ -2,14 +2,22 @@ import { client, topic} from "../../Connection";
 import './button.css';
 
 export default function Button() {
-    function mandaMSG(mensagem) {
-        client.publish(topic, mensagem)
+    function mandaMSG(topico, mensagem) {
+        client.publish(topico, mensagem)
     }
 
     return(
         <div className="div-button">
-            <button onClick={()=> {mandaMSG('Desliga rele')}}>Liga quarto</button>
-            <button onClick={()=> {mandaMSG('Liga rele')}}>Desliga quarto</button>
+            <div className="button-individual">
+                <button onClick={()=> {mandaMSG(topic,'Liga rele')}}>Liga quarto</button>
+                <button onClick={()=> {mandaMSG(topic, 'Desliga rele')}}>Desliga quarto</button>
+            </div>
+            
+            <div className="button-individual">
+                <button onClick={()=> {mandaMSG((topic + '1'),'Liga rele')}}>Liga quarto PC</button>
+                <button onClick={()=> {mandaMSG(topic + '1', 'Desliga rele')}}>Desliga quarto PC</button>
+            </div>
+            
         </div>
     )
 }
