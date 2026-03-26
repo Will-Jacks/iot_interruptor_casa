15bdd87349e54435c057088cd403b7cd7950df43
William Jackson <lila.lilajackson@hotmail.com>
2026-03-26 15:47:57 -0300

    refactoring the css app

--- src/Components/Buttons/Buttons.jsx
+++ src/Components/Buttons/Buttons.jsx
@@ -1,28 +1,27 @@
-import { client, topic} from "../../Connection";
-import './button.css';
+import { useState } from "react";
+import { client, topic } from "../../Connection";
+import "./button.css";
 
 export default function Button() {
-    function mandaMSG(topico, mensagem) {
-        client.publish(topico, mensagem)
-    }
+  const [isOn, setIsOn] = useState(true);
 
-    return(
-        <div className="div-button">
-                <div className="button-individual">
-                    <button onClick={()=> {mandaMSG(topic,'Liga rele')}}>👫🏻 💡</button>
-                    <button onClick={()=> {mandaMSG(topic, 'Desliga rele')}}>👫🏻</button>
-                </div>
-            
-                <div className="button-individual">
-                    <button onClick={()=> {mandaMSG((topic + '1'),'Liga rele')}}>🖥 💡</button>
-                    <button onClick={()=> {mandaMSG(topic + '1', 'Desliga rele')}}>🖥</button>
-                </div>
-                <div className="button-individual">
-                    <button onClick={()=> {mandaMSG((topic + '12'),'Liga rele')}}>🛋️ 💡</button>
-                    <button onClick={()=> {mandaMSG(topic + '12', 'Desliga rele')}}>🛋️</button>
-                </div>
-            
+  function mandaMSG(topico, mensagem) {
+    client.publish(topico, mensagem);
+    setIsOn(!isOn);
+  }
+  return (
+    <div className="div-button">
+      <button
+        className={isOn ? "inactive" : "active"}
+        onClick={() => {
+          mandaMSG(topic, isOn ? "Desligar luz" : "Ligar luz");
+        }}
+      >
+        <span>{isOn ? "Desligado" : "Ligado"}</span>
+        <div style={{ fontSize: "10px", opacity: 0.7 }}>
+          Clique para alternar
         </div>
-        
-    )
+      </button>
+    </div>
+  );
 }
